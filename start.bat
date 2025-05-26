35514cabd5f02c2c17843ce47561ce85177070df
DmtrSrgvchM <crix.1999.03@gmail.com>
2025-05-26 20:29:29 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,6 @@
-#!/bin/bash
-python bot.py
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/ТВОЙ_ПОЛЬЗОВАТЕЛЬ/my-telegram-bot.git
+git push -u origin main
